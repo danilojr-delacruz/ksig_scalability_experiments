6bcff5b733f566eb55167052308abf54a776ec32
Danilo Jr Dela Cruz <delacruz.danilojr@gmail.com>
2025-06-05 13:52:33 +0100

    Implement experiments for sigkerax.

---
+++ sigkerax/base_script.py
@@ -0,0 +1,126 @@
+import os
+import time
+import gc
+import tracemalloc
+import threading
+
+import jax
+from sigkerax.sigkernel import SigKernel
+
+# Ensure we use GPU-0
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+EMBEDDING_ORDER = 1
+N = 10
+d = 5
+
+M = 5
+n_components_rff = 100
+n_components_proj = 100
+
+
+################################################################################
+def monitor_time(function, X, *args, **kwargs):
+    # Just use CPU time, they are the same
+    start_cpu = time.perf_counter()
+
+    kernel = function(X, *args, **kwargs)
+    K = kernel.kernel_matrix(X, X)
+    _ = jax.block_until_ready(K)
+
+    end_cpu = time.perf_counter()
+    t_cpu = end_cpu - start_cpu
+
+    del kernel
+    del K
+    gc.collect()
+
+    return t_cpu, t_cpu
+
+
+def monitor_cpu_memory(function, X, *args, **kwargs):
+    tracemalloc.start()
+    tracemalloc.reset_peak()
+
+    kernel = function(X, *args, **kwargs)
+    K = kernel.kernel_matrix(X, X)
+    _ = jax.block_until_ready(K)
+
+    current, peak = tracemalloc.get_traced_memory()
+
+    cpu_current = current
+    cpu_peak = peak
+
+    tracemalloc.stop()
+
+    del kernel
+    del K
+    gc.collect()
+
+    return cpu_current, cpu_peak
+
+
+def gpu_mem_used(id):
+    return int(jax.local_devices()[0].memory_stats()["peak_bytes_in_use"])
+
+def gpu_mem_used_no_cache(id):
+    # Need to do jax.block_until_ready?
+    return gpu_mem_used(id)
+
+def peak_monitor_start():
+    global peak_monitoring
+    peak_monitoring = True
+
+    # this thread samples RAM usage as long as the current epoch of the fit loop is running
+    peak_monitor_thread = threading.Thread(target=peak_monitor_func)
+    peak_monitor_thread.daemon = True
+    peak_monitor_thread.start()
+
+def peak_monitor_stop():
+    global peak_monitoring
+    peak_monitoring = False
+
+def peak_monitor_func():
+    global nvml_peak, peak_monitoring
+    nvml_peak = 0
+    id = 0
+
+    while True:
+        nvml_peak = max(gpu_mem_used(id), nvml_peak)
+        if not peak_monitoring: break
+        time.sleep(0.001) # 1msec
+
+
+def monitor_gpu_memory(function, X, *args, **kwargs):
+    global peak_monitoring, nvml_peak
+    # Global variables
+    peak_monitoring = False
+    nvml_peak = 0
+
+    id = 0
+    nvml_before = gpu_mem_used_no_cache(id)
+
+    peak_monitor_start()
+
+    kernel = function(X, *args, **kwargs)
+    K = kernel.kernel_matrix(X, X)
+    jax.block_until_ready(K)
+
+    # code finished
+    peak_monitor_stop()
+    nvml_after = gpu_mem_used_no_cache(id)
+
+    gpu_current = nvml_after
+    gpu_peak = nvml_peak-nvml_before
+
+    del kernel
+    del K
+    gc.collect()
+
+    return gpu_current, gpu_peak
+
+################################################################################
+def sigkerax(X, **kwargs):
+    signature_kernel = SigKernel(static_kernel_kind="rbf")
+    return signature_kernel
+################################################################################
---
+++ sigkerax/sequence_length_time.py
@@ -0,0 +1,97 @@
+import numpy as np
+import time
+import gc
+import pickle
+import subprocess
+
+import jax
+from sigkerax.sigkernel import SigKernel
+
+EMBEDDING_ORDER = 1
+OUTPUT_DIR = "results/sequence_length_time/"
+NUM_RUNS = 11
+
+# In seconds, 10 minutes
+MAX_RUN_TIME = 600
+
+
+###############################################################################
+functions_to_call = [
+    "sigkerax"
+]
+
+blacklisted = dict()
+for function in functions_to_call:
+    blacklisted[function] = False
+
+results = dict()
+
+def get_function_call_code(L, name):
+    return f"""L = {L}
+# Generate X, Y
+key = jax.random.split(jax.random.PRNGKey(0), num=1)
+X = jax.random.normal(key[0], shape=(N, L, d))
+X = X.cumsum(axis=1) / jax.numpy.sqrt(L)
+
+function = {name}
+
+cpu_time, gpu_time = monitor_time(function, X, n_levels=M,
+                n_components_rff=n_components_rff,
+                n_components_proj=n_components_proj
+            )
+
+print(cpu_time, gpu_time)
+"""
+
+
+with open("sigkerax/base_script.py", "r") as f:
+    base_script = f.read()
+
+for x in np.linspace(2, 6, num=4*5 + 1):
+    L = int(10 ** x)
+    print(L)
+    for name in functions_to_call:
+
+        function_call_code = get_function_call_code(L, name)
+        script = base_script + "\n" + function_call_code
+        print(name)
+        results[("cpu", name, L)] = []
+        results[("gpu", name, L)] = []
+
+        for i in range(NUM_RUNS):
+            if blacklisted[name]:
+                print(i, "blacklisted")
+                results[("cpu", name, L)].append(None)
+                results[("gpu", name, L)].append(None)
+                continue
+
+            try:
+                result = subprocess.run(['python3', '-c', script],
+                                        capture_output=True, text=True,
+                                        timeout=MAX_RUN_TIME)
+                if result.returncode == 0:
+                    cpu_time, gpu_time = map(
+                    float, result.stdout.strip().split(" ")
+                )
+                    print(i, "ran in", cpu_time, gpu_time)
+                    results[("cpu", name, L)].append(cpu_time)
+                    results[("gpu", name, L)].append(gpu_time)
+                else:
+                    print(i, "Script failed")
+                    # Don't try it for larger cases
+                    blacklisted[name] = True
+                    results[("cpu", name, L)].append(None)
+                    results[("gpu", name, L)].append(None)
+            except subprocess.TimeoutExpired:
+                print(i, "Timeout")
+                blacklisted[name] = True
+                results[("cpu", name, L)].append("Timeout")
+                results[("gpu", name, L)].append("Timeout")
+        print()
+
+    print()
+
+print(results)
+
+with open(f"{OUTPUT_DIR}/brownian_sigkerax.pkl", "wb") as f:
+    pickle.dump(results, f)
